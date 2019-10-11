07e24740bc68ff19cbbeb73ead333ffb56b9fe08
samina <nimrazulfikar116@gmail.com>
2019-10-11 20:33:29 +0500

    my first commit

---
+++ assignment6.py
@@ -0,0 +1,65 @@
+print('hello, world!')
+print(17)
+print(3.2)
+message='And now for something completely different'
+n=17
+pi=3.14
+print(n)
+print(pi)
+print(1)
+x=2
+print(x)
+
+first=10
+second=15
+print(first+second)
+
+first='test'
+second=3
+print(first*second)
+
+a=35.0
+b=12.50
+c=a*b
+print(c)
+
+name=input
+print(name)
+
+hours=35
+rate=12.50
+hours=int(input("enter hours: "))
+rate=float(input("enter rate: "))
+salary=hours*rate
+print("pay: {0:.2f}" .format(salary))
+
+width=17
+height=12.0
+
+
+print(width//2)
+print(type(width//2))
+print(width/2.0)
+print(type(width/2.0))
+print(height/3)
+print(type(height/3))
+print(1+2*5)
+print(type(1+2*5))
+
+print(height/3)
+print(1+2*5)
+
+celsius=float(input("Enter degrees in celsius:\n"))
+fahrenheit=(celsius*(9/5))+32
+print(fahrenheit)
+
+
+
+
+
+
+
+
+
+
+
